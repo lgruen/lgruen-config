9ca353a018b48b7d43ec98a877caf8898fadf1ad
Leonhard Gruenschloss <leo@Leonhards-MacBook-Pro.local>
2023-06-10 15:41:38 +1000

    Fix disablenetwork.py for python3

--- disablenetwork.py
+++ disablenetwork.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python2.7
-import subprocess, re
+#!/usr/bin/env python3
+import subprocess, re, sys
 services = re.findall("\(\d+?\) (.+?)\n\(Hardware Port: (.+?), Device: (.+?)\)\n",
-  subprocess.check_output(["/usr/sbin/networksetup", "-listnetworkserviceorder"]))
+  subprocess.check_output(["/usr/sbin/networksetup", "-listnetworkserviceorder"]).decode(sys.stdout.encoding))
 for service in services:
   if service[1] == "Wi-Fi":
     subprocess.check_output(["/usr/sbin/networksetup", "-setairportpower", service[2], "off"])
